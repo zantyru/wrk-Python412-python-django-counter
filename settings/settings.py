7595cc71020f72035d4e3209c91591215b1f64b7
Evgeny Zaonegin <none@no.ne>
2025-09-23 22:27:38 +0300

    Настраивает STATIC_ROOT
    :q

--- settings/settings.py
+++ settings/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+# STATICFILE_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
